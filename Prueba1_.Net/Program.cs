ba2764c00c346cf5b2a3b5cdcaf657f0fe47c8f0
Luis Aroca Rosales <luis.aroca3@gmail.com>
2020-05-17 03:34:13 -0400

    dddd

---
+++ Prueba1_.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prueba1_.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
